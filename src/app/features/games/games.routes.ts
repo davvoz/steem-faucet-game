3489c8682b8498d6934ef6fa6f4ebb1ec7a0f394
David <dav.baz@alice.it>
2025-04-20 10:34:08 +0200

    refactor: remove memory game and quiz game components and styles

--- src/app/features/games/games.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { GamesComponent } from './games.component';
-
-export const GAMES_ROUTES: Routes = [
-  {
-    path: '',
-    component: GamesComponent
-  }
-];
--- src/app/features/leaderboard/leaderboard.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { LeaderboardComponent } from './leaderboard.component';
-
-export const LEADERBOARD_ROUTES: Routes = [
-  {
-    path: '',
-    component: LeaderboardComponent
-  }
-];
